016fd643e94d059f94d16c10870886a334cbac78
Mark Allison <mark@stylingandroid.com>
2019-05-25 11:52:12 +0100

    Code for EdgeCases article

--- app/src/main/java/com/stylingandroid/gesturenav/MainActivity.kt
+++ app/src/main/java/com/stylingandroid/gesturenav/MainActivity.kt
@@ -17,10 +17,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
-//        window.decorView.systemUiVisibility =
-//            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-//                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
