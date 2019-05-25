0d5353ff9884ff7f7c1f49fb3829ff5d3c28c6f3
Mark Allison <mark@stylingandroid.com>
2019-05-25 16:56:41 +0100

    Code for Window Insets article

--- app/src/main/java/com/stylingandroid/gesturenav/MainActivity.kt
+++ app/src/main/java/com/stylingandroid/gesturenav/MainActivity.kt
@@ -2,9 +2,12 @@ package com.stylingandroid.gesturenav
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavArgument
+import androidx.navigation.NavType
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
@@ -17,24 +20,43 @@ class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        window.decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
 
-        val toolbar: Toolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(toolbar)
+        setupToolbar()
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
 
         val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_home, R.id.nav_gallery
-            ), drawerLayout
-        )
+        appBarConfiguration =
+            AppBarConfiguration(setOf(R.id.nav_home, R.id.nav_gallery), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        drawerLayout.setOnApplyWindowInsetsListener { _, windowInsets ->
+            navController.graph.findNode(R.id.nav_gallery)?.addArgument(
+                "BottomPadding",
+                NavArgument.Builder()
+                    .setType(NavType.IntType)
+                    .setDefaultValue(windowInsets.systemWindowInsetBottom)
+                    .build()
+            )
+            windowInsets
+        }
+    }
+
+    private fun setupToolbar() {
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
--- app/src/main/java/com/stylingandroid/gesturenav/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/stylingandroid/gesturenav/ui/gallery/GalleryFragment.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.updatePadding
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.stylingandroid.gesturenav.R
+import kotlinx.android.synthetic.main.fragment_gallery.view.*
 
 class GalleryFragment : Fragment() {
 
@@ -13,5 +17,10 @@ class GalleryFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.fragment_gallery, container, false)
+    ): View? = inflater.inflate(R.layout.fragment_gallery, container, false).also {
+        arguments?.getInt("BottomPadding", 0)?.also { bottomPadding ->
+            it.updatePadding(bottom = bottomPadding)
+        }
+        it.navigation_view.setupWithNavController(findNavController())
+    }
 }
